9ed07b15ad083e3267811c5158ffa60f8b1eb603
aluno <aluno@10.107.71.36>
2022-04-18 10:51:40 -0300

    v 2.0

--- calculaArea/src/calculaArea/Aplicacao.java
+++ calculaArea/src/calculaArea/Aplicacao.java
@@ -7,27 +7,53 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
+		CalculaArea A = new CalculaArea();
 		
-		System.out.printf("Digite os lados do quadrado: %n");
-		System.out.printf("x:");
-		int q1 = teclado.nextInt();
-		System.out.printf("y:");
-		int q2 = teclado.nextInt();
-		System.out.printf("A área do quadrado é: %d%n", q1*q2);
+		System.out.println("Você quer calcular a área de qual figura ? ");
+		int resposta = teclado.nextInt();
 		
-		System.out.printf("Digite os lados do retângulo: %n");
-		System.out.printf("x:");
-		int r1 = teclado.nextInt();
-		System.out.printf("y:");
-		int r2 = teclado.nextInt();
-		System.out.printf("A área do retângulo é: %d %n", r1*r2);
+		System.out.println("1 - Quadrado");
+		System.out.println("2 - Retângulo");
+		System.out.println("3 - Triângulo");
 		
-		System.out.printf("Digite os paramêtros do triângulo: %n");
-		System.out.printf("Base:");
-		int tb = teclado.nextInt();
-		System.out.printf("Altura:");
-		int th = teclado.nextInt();
-		System.out.printf("A área do triângulo é: %d", (tb*th)/2);
+		if(resposta == 1) {
+			int x = teclado.nextInt();
+			A.calculaAe(x);
+			
+		}else if(resposta == 2) {
+			int x = teclado.nextInt();
+			int y = teclado.nextInt();
+			A.calculaAe(x, y);
+			
+		}else {
+			double x = teclado.nextInt();
+			double y = teclado.nextInt();
+			
+			A.calculaAe(x, y);
+		}
+		
+		
+		
+		//System.out.printf("Digite os lados do quadrado: %n");
+		//System.out.printf("x:");
+		//int q1 = teclado.nextInt();
+		//System.out.printf("y:");
+		//int q2 = teclado.nextInt();
+		//System.out.printf("A área do quadrado é: %d%n", q1*q2);
+		
+		//System.out.printf("Digite os lados do retângulo: %n");
+		//System.out.printf("x:");
+		//int r1 = teclado.nextInt();
+		//System.out.printf("y:");
+		//int r2 = teclado.nextInt();
+		//System.out.printf("A área do retângulo é: %d %n", r1*r2);
+		
+		//System.out.printf("Digite os paramêtros do triângulo: %n");
+		//System.out.printf("Base:");
+		//int tb = teclado.nextInt();
+		//System.out.printf("Altura:");
+		//int th = teclado.nextInt();
+		//System.out.printf("A área do triângulo é: %d", (tb*th)/2);
 		
 		
 	}
---
+++ calculaArea/src/calculaArea/CalculaArea.java
@@ -0,0 +1,13 @@
+package calculaArea;
+
+public class CalculaArea {
+	int calculaAe(int x, int y) {
+		return x*y;
+	}
+	int calculaAe(int x) {
+		return x*x;
+	}
+	double calculaAe(double x, double y) {
+		return (x*y)/2;
+	}
+}
