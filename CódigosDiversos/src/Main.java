cc2d7ca329400b0ab6e1ee0c2511752cd485aaea
aluno <aluno@DSN1251017053>
2022-04-27 08:31:22 -0300

    Tabuada

--- CódigosDiversos/src/Main.java
+++ CódigosDiversos/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Qual número você quer multiplicar? ");
+		int y= entrada.nextInt();
 		
+		for(int x=0; x<=10; x++) {
+			System.out.printf("%d x %d = %d%n", y, x, y*x);
+		}
 
 	}
 
