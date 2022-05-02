255f57743b623eb6d52cb2344d8c7a634a73359b
aluno <aluno@DSN1251017053>
2022-05-02 10:38:40 -0300

    v 1.0

---
+++ ElementosEstaticos/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Matematica matematica = new Matematica();
+		
+		int resultado = matematica.somar(151356425, 352122135);
+		
+		System.out.println(resultado);
+	}
+
+}
---
+++ ElementosEstaticos/src/Matematica.java
@@ -0,0 +1,11 @@
+public class Matematica {
+	
+	public int somar(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtrair(int a, int b) {
+		return a - b;
+	}
+	
+}
