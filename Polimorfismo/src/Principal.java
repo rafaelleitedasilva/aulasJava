c56398ed6797ff3c3292d57002ff30343e936952
aluno <aluno@DSN1251017053>
2022-05-04 11:30:27 -0300

    v 1.1

--- Polimorfismo/src/Principal.java
+++ Polimorfismo/src/Principal.java
@@ -3,13 +3,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Gato gato = new Gato();
-		Cachorro cão = new Cachorro();
+//		Gato gato = new Gato();
+		//Generic class
+		Animal animal = new Cachorro();
+//		Cachorro cao = new Cachorro();		
+//		gato.Falar();
+//		cao.Falar();
+//		cao.Morder();
+		animal.Falar();
 		
-		
-		gato.Falar();
-		cão.Falar();
-		cão.Morder();
+//		Poliformism
+		animal = new Gato();
+		animal.Falar();
 		
 	}
 
