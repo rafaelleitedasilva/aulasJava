ea988b154f4204f6c81f9fd88dea36ad273b29b0
aluno <aluno@DSN1251017053>
2022-05-09 10:58:28 -0300

    v 1.5

--- PolimorfismoBebidas/src/Preparador.java
+++ PolimorfismoBebidas/src/Preparador.java
@@ -3,9 +3,9 @@ public class Preparador {
 	
 	public void prepararBebida(Bebida bebida) {
 		bebida.prepara();
-		System.out.printf("Colocando %s no copo%n", bebida.getNome());
+		System.out.printf("Colocando %s no copo!%n", bebida.getNome());
 		if(bebida.isQuente()) {
-			System.out.printf("Aquecendo a bebida %s!%n", bebida.getNome());
+			System.out.printf("Aquecendo a bebida: %s!%n", bebida.getNome());
 		}
 		
 		System.out.printf("Bebida preparada ;P %n%n");
