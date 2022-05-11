38c3af535e98b41641877ec7ca10eca86906c167
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-11 10:04:04 -0300

    v 1.0 11/05

--- Interfaces/src/Aplicacao.java
+++ Interfaces/src/Aplicacao.java
@@ -4,11 +4,17 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		
 		Rota rota = new Rota();
-		Automovel Ferrari = new Ferrari();
+		Ferrari Ferrari = new Ferrari();
 		Automovel Fusca = new Fusca();
 		
 		rota.rotaAlmanaque(Ferrari);
 		rota.rotaAlmanaque(Fusca);
+		
+		Vendedor vendedor = new Vendedor();
+		Apartamento apartamento = new Apartamento();
+		
+		vendedor.mostrarPreco(apartamento);
+		vendedor.mostrarPreco(Ferrari);
 		
 
 	}
---
+++ Interfaces/src/Vendedor.java
@@ -0,0 +1,6 @@
+
+public class Vendedor {
+	public void mostrarPreco(ItemCaro caro) {
+		System.out.printf("O valor é R$%.2f%n", caro.getPreco());
+	}
+}
