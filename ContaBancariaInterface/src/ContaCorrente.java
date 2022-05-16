9a6b959f7738d7a833d95367963b9996f012cdcf
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-16 10:57:49 -0300

    16/05/2022

--- ContaBancariaInterface/src/ContaCorrente.java
+++ ContaBancariaInterface/src/ContaCorrente.java
@@ -1,14 +1,19 @@
 
 public class ContaCorrente extends ContaBancaria{
 	
+	//O saldo da conta corrente é igual ao da conta bancária
 	private double saldo = ContaBancaria.getSaldo();
 
+	
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
 	
 	@Override
+	//Fazendo o override dos métodos do conta bancária
+	//já que estou tratando as extends como contas separadas.
+	
 	public void depositar(double valor) {
 		// TODO Auto-generated method stub
 		saldo +=valor;
