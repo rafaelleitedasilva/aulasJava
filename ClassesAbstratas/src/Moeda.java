cf7318f48f4057bde8102b72495bbb1d629df2f4
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-22 18:42:29 -0300

    22/05/2022

--- ClassesAbstratas/src/Moeda.java
+++ ClassesAbstratas/src/Moeda.java
@@ -1,5 +1,6 @@
 
 public class Moeda extends Item {
+	//Override da assinatura de método com "super.pegar()" que garante trazer os elementos que já estavam no método da classe abstrata principal.
 	@Override
 	public void pegar() {
 		// TODO Auto-generated method stub
