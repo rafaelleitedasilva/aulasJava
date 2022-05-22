b80f3a176315b05b7f92376682b251c512a05fea
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-22 19:20:22 -0300

    22/05/2022

--- DataHora/src/Data.java
+++ DataHora/src/Data.java
@@ -1,5 +1,6 @@
 
 public class Data {
+	//Atributos privados que só serâo modificados durante a construção da classe
 	private int hora = -1;
 	private int minuto;
 	private int segundo;
@@ -9,6 +10,8 @@ public class Data {
 	public static final boolean FORMATO_12H = true;
 	public static final boolean FORMATO_24H = false;
 	
+
+	//Construtor para apenas dia, mes e ano
 	public Data(int dia, int mes, int ano) {
 		super();
 		this.dia = dia;
@@ -16,6 +19,7 @@ public class Data {
 		this.ano = ano;
 	}
 
+	//Construtor para dia, mes, ano, hora, minuto e segundo
 	public Data(int dia, int mes, int ano, int hora, int minuto, int segundo) {
 		super();
 		this.hora = hora;
@@ -26,15 +30,19 @@ public class Data {
 		this.ano = ano;
 	}
 	
+	//Imprimindo a data com diferentes formatos
 	public void Imprimir(boolean formato) {
-		
+		//Se o formato for 12Horas e eu tiver um horário menor que meio dia eu apenas coloco AM e as horas
 		if(formato == true && hora >= 0 && hora<=12) {
 			System.out.printf("%d/%d/%d %d:%d:%d AM %n", dia, mes, ano, hora, minuto, segundo);
-		}else if(formato == true && hora >= 0 && hora <= 23 && hora>12) {
+		}//Se o foramto for de 12horas e a hora for maior que meio dia eu faço a divisão por resto da hora
+		else if(formato == true && hora >= 0 && hora <= 23 && hora>12) {
 			System.out.printf("%d/%d/%d %d:%d:%d PM %n", dia, mes, ano, hora%12, minuto, segundo);
-		}else if (formato == false && hora>=0 && hora<23){
+		}//Se o formato for 24horas eu apenas mostro as informações
+		else if (formato == false && hora>=0 && hora<23){
 			System.out.printf("%d/%d/%d %d:%d:%d %n", dia, mes, ano, hora%24, minuto, segundo);
-		}else if(hora == -1) {
+		}//E se a hora não for setada eu mostro apenas o dia, mes e ano
+		else if(hora == -1) {
 			System.out.printf("%d/%d/%d %n", dia, mes, ano);
 		}
 	}
--- DataHora/src/Principal.java
+++ DataHora/src/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Eu instancio uma data com dia, mês, ano e hora
 		Data d1 = new Data(20, 03, 2000, 21, 2, 3);
+		//Imprimindo a data com difentes formatos
 		d1.Imprimir(d1.FORMATO_12H);
 		d1.Imprimir(d1.FORMATO_24H);
 	}
