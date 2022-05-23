315de2940e6e0c2ee1bdcb5039e6d5f3aee86e48
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-23 08:51:45 -0300

    23/05/2022

--- Enumarete/src/DiaDaSemana.java
+++ Enumarete/src/DiaDaSemana.java
@@ -1,10 +1,33 @@
 
 public enum DiaDaSemana {
-	DOMINGO,
-	SEGUNDA,
-	TERCA,
-	QUARTA,
-	QUINTA,
-	SEXTA,
-	SABADO;
+	
+	//O valor entre os parenteses e o valor que esta associado a constante
+	//Cada uma dessas constantes vai virar um objeto
+	
+	//Como o enum e um tipo de dado podemos ter atributos e metodos
+	
+	DOMINGO (1),
+	SEGUNDA (2),
+	TERCA (3),
+	QUARTA (4),
+	QUINTA (5),
+	SEXTA (6),
+	SABADO (7);
+	
+	
+	//Como o atributo e privado precisamos criar o get
+	//Cada constante possui o seu valor inteiro pre-setado
+	//Conseguimos retornar esse valor, mas nao setar um valor por cima
+	public int getNum() {
+		return num;
+	}
+
+	//Para setar o valor do atributo para cada constante eu preciso criar um construtor
+	private int num;
+
+	private DiaDaSemana(int num) {
+		this.num = num;
+	}
+	
+	
 }
--- Enumarete/src/Main.java
+++ Enumarete/src/Main.java
@@ -1,9 +1,44 @@
 
 public class Main {
-
+	
+	//Cosntante do tipo primitivo
+	public static final double PI = 3.14;
+	//No enumerate nos conseguimos ter um conjunto maior de valores para um unico enum
+	
+	private enum Sexo{
+		MASCULINO, FEMININO;
+		
+	}
+	
 	public static void main(String[] args) {
 		
+		//Chamando a constante e guardando o seu valor em uma variavel
+		double numeroPi = Main.PI;
 		
+		//Utilizando a constante do enumerate
+		DiaDaSemana hoje = DiaDaSemana.SEGUNDA;
+		System.out.println(hoje);
+		
+		//Acessando o metodo pela constante SEGUNDA e retornando o valor inteiro armazenado nessa constante
+		int num = DiaDaSemana.SEGUNDA.getNum();
+		
+		//criado uma variavel string com o valor TERCA
+		String s = "TERCA";
+		
+		//Metodo de enum que permite converter valores na constante
+		//e mudar um enum SEGUNDA para TERCA por exemplo
+		hoje = DiaDaSemana.valueOf(s);
+		
+		//Mostrando o resultado das variaveis
+		System.out.println(hoje);
+		System.out.println(hoje.getNum());
+		System.out.println(num);
+		
+		//Utilizando o enum criado no proprio arquivo main
+		Sexo sexo = Sexo.MASCULINO;
+		sexo = Sexo.FEMININO;
+		
+		System.out.println(sexo);
 	}
 
 }
