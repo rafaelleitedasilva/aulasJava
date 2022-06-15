52bdb5009519a04d8019dd3400f06afad6baebe8
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-06-15 08:47:54 -0300

    15/06/2022

---
+++ JavaFX/src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,1920,1020);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			GridPane grid = new GridPane();
+			grid.setHgap(5.0);
+			grid.setVgap(7.0);
+			
+			
+			
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ JavaFX/src/module-info.java
@@ -0,0 +1,5 @@
+module JavaFX {
+	requires javafx.controls;
+	requires javafx.graphics;
+	opens application to javafx.graphics, javafx.fxml;
+}
