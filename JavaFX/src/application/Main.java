5ac4028dd2deeb6f69b6905d8061a7dcda358a2c
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-06-15 10:30:59 -0300

    15/06/2022

--- JavaFX/src/application/Main.java
+++ JavaFX/src/application/Main.java
@@ -1,25 +1,30 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//Colocando o título da Interface
-			
 			
 			//Criando o Painel do Grid
 			GridPane grid = new GridPane();
@@ -37,6 +42,14 @@ public class Main extends Application {
 			grid.add(new Label("Sexo: "), 0, 1);
 			RadioButton btnMasc = new RadioButton("Masculino");
 			RadioButton btnFem = new RadioButton("Feminino");
+			
+			//Colocando Select and Option
+			ChoiceBox<String> chcEstados =  new ChoiceBox<>();
+			ObservableList<String> estados = FXCollections.observableArrayList("São Paulo", "Rio de Janeiro", "Minas Gerais");
+			chcEstados.setItems(estados);
+			 
+			grid.add((chcEstados), 0, 2);
+			
 			//Deixando o sexo Masculino já selecionado
 			btnMasc.setSelected(true);
 			
@@ -44,14 +57,31 @@ public class Main extends Application {
 			ToggleGroup group = new ToggleGroup();
 			btnFem.setToggleGroup(group);
 			btnMasc.setToggleGroup(group);
-			//Adicionando os botões no grid
 			
+			//Juntando os botões em uma mesma caixa
+			HBox radioBox = new HBox(5.0, btnMasc, btnFem);
 			
-			//Adicionando o grid na tela
+			grid.add((radioBox), 1, 1);
+			
+			Label lblTeste = new Label();
+			grid.add((lblTeste), 0, 3);
+			
+			//Criando os botões
+			Button btnTeste = new Button("Enviar");
+			btnTeste.setOnAction(event -> lblTeste.setText(txtNome.getText()));
+			grid.add((btnTeste), 1, 2);
+			
+			//Alinhando a Grid
+			grid.setAlignment(Pos.CENTER); 
+			
+			//Adicionando o grid no root
 			root.setCenter(grid);
 			
+			//Colocando a tela maximizada
 			primaryStage.setMaximized(true);
 			primaryStage.setScene(scene);
+			
+			//Colocando o título da Interface
 			primaryStage.setTitle("Primeira Interface");
 			primaryStage.show();
 		} catch(Exception e) {
